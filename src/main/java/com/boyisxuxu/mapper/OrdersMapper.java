be42af2dc35bef53bd50a32e6f360c20367d40d9
Zhengfangxing <475164744@qq.com>
2018-05-31 03:30:47 +0800

    IDEA开发MyBatis逆向工程

---
+++ src/main/java/com/boyisxuxu/mapper/OrdersMapper.java
@@ -0,0 +1,17 @@
+package com.boyisxuxu.mapper;
+
+import com.boyisxuxu.po.Orders;
+
+public interface OrdersMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    Orders selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
---
+++ src/main/java/com/boyisxuxu/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.boyisxuxu.mapper;
+
+import com.boyisxuxu.po.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
